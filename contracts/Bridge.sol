946aaaa169fa3fdf6bfc345ac96edd97d6ac0989
Danila Pisarev <jobdan@bk.ru>
2022-03-23 17:45:43 +0300

    Added tests for redeem

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1,14 +1,16 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+// TODO: delete
+import "hardhat/console.sol";
+
 import "./ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Bridge is AccessControl {
-    // TODO: make private this variables
     event SwapInitialized(
         address indexed sender,
-        address indexed recepient,
+        address indexed recipient,
         uint256 amount,
         uint256 nonce
     );
@@ -16,8 +18,9 @@ contract Bridge is AccessControl {
     ERC20 token;
     address private validator;
 
-    constructor() {
+    constructor(ERC20 _token) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        token = _token;
     }
 
     function setValidator(address _validator) public {
@@ -26,7 +29,7 @@ contract Bridge is AccessControl {
     }
 
     function getMsgHash(
-        address recepient,
+        address recipient,
         uint256 amount,
         uint256 chainFrom,
         uint256 chainTo,
@@ -36,7 +39,7 @@ contract Bridge is AccessControl {
         return
             keccak256(
                 abi.encodePacked(
-                    recepient,
+                    recipient,
                     amount,
                     chainFrom,
                     chainTo,
@@ -47,16 +50,15 @@ contract Bridge is AccessControl {
     }
 
     function swap(
-        address recepient,
+        address recipient,
         uint256 amount,
         uint256 chainFrom,
         uint256 chainTo,
         string memory symbol,
         uint256 nonce
     ) public {
-        // Сжигает токены
         bytes32 msgHash = getMsgHash(
-            recepient,
+            recipient,
             amount,
             chainFrom,
             chainTo,
@@ -64,21 +66,31 @@ contract Bridge is AccessControl {
             nonce
         );
         require(!transactionsHash[msgHash], "Existing transaction");
-        // Вносить хеш из параметров в мапу, вот тут и нужен nonce
         transactionsHash[msgHash] = true;
         token.burn(msg.sender, amount);
-        // Отправляем ивент
-        emit SwapInitialized(msg.sender, recepient, amount, nonce);
+        emit SwapInitialized(msg.sender, recipient, amount, nonce);
     }
 
-    function getEthMsgHash(bytes32 _msgHash) public pure returns (bytes32) {
-        string memory prefixer = "\x19Ethereum Signed Message:\n32";
-        return keccak256(abi.encodePacked(prefixer, _msgHash));
+    function getEthMsgHash(bytes32 _msgHash) internal pure returns (bytes32) {
+        string memory prefix = "\x19Ethereum Signed Message:\n32";
+        return keccak256(abi.encodePacked(prefix, _msgHash));
     }
 
+    /**
+        @notice Calls this function to receive sended tokens from chain with "chainFrom" id to chain with "chainTo" id
+        @dev We need to create message using "\x19Ethereum Signed Message:\n32"
+        @param recipient Address that receive tokens from chain with "chainFrom" id
+        @param amount Amount of receiving tokens
+        @param chainFrom Chain id from wich tokens are sent
+        @param chainTo Chain id to wich tokens are sent
+        @param symbol Symbol of sended tokens
+        @param nonce Some number from creating message hash
+        @param v Recovery id 
+        @param r Output of the ECDSA signature
+        @param s Output of the ECDSA signature
+     */
     function redeem(
-        address owner,
-        address recepient,
+        address recipient,
         uint256 amount,
         uint256 chainFrom,
         uint256 chainTo,
@@ -86,11 +98,10 @@ contract Bridge is AccessControl {
         uint256 nonce,
         uint8 v,
         bytes32 r,
-        bytes32 s 
-        // bytes calldata signature
+        bytes32 s
     ) public {
         bytes32 msgHash = getMsgHash(
-            recepient,
+            recipient,
             amount,
             chainFrom,
             chainTo,
@@ -99,12 +110,19 @@ contract Bridge is AccessControl {
         );
         require(!transactionsHash[msgHash], "Existing transaction");
         bytes32 ethMsgHash = getEthMsgHash(msgHash);
-        require(checkSign(ethMsgHash, v, r, s), "Not valid signature");
-
+        require(checkSign(ethMsgHash, v, r, s), "Invalid signature");
         transactionsHash[msgHash] = true;
-        token.mint(owner, amount);
+        token.mint(recipient, amount);
     }
 
+    /**
+        @notice Checks that signer of the message is validator
+        @dev Recover the signer of the message using ecrecover
+        @param ethMsgHash Ethereum signed message
+        @param v Recovery id 
+        @param r Output of the ECDSA signature
+        @param s Output of the ECDSA signature
+     */
     function checkSign(
         bytes32 ethMsgHash,
         uint8 v,
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,186 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Bridge, Bridge__factory, ERC20, ERC20__factory } from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Bridge", function () {
+  let ethBridge: Bridge,
+    bscBridge: Bridge,
+    ethToken: ERC20,
+    bscToken: ERC20,
+    owner: SignerWithAddress,
+    backend: SignerWithAddress,
+    acc1: SignerWithAddress;
+  const ETHER_CHAIN_ID = 1;
+  const BSC_CHAIN_ID = 2;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async () => {
+    [owner, backend, acc1] = await ethers.getSigners();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const EthTokenFactory = (await ethers.getContractFactory(
+      "ERC20"
+    )) as ERC20__factory;
+    const BscTokenFactory = (await ethers.getContractFactory(
+      "ERC20"
+    )) as ERC20__factory;
+    ethToken = await EthTokenFactory.deploy("ETHER TOKEN", "ETHT", 2);
+    bscToken = await BscTokenFactory.deploy("BINANCE TOKEN", "BSCT", 2);
+    await ethToken.deployed();
+    await ethToken.mint(owner.address, ethers.utils.parseEther("1"));
+    expect(await ethToken.balanceOf(owner.address)).to.equal(
+      ethers.utils.parseEther("1")
+    );
+    await bscToken.deployed();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const EthBridgeFactory = (await ethers.getContractFactory(
+      "Bridge"
+    )) as Bridge__factory;
+    const BscBridgeFactory = (await ethers.getContractFactory(
+      "Bridge"
+    )) as Bridge__factory;
+    ethBridge = (await EthBridgeFactory.deploy(ethToken.address)) as Bridge;
+    bscBridge = (await BscBridgeFactory.deploy(bscToken.address)) as Bridge;
+    await ethBridge.deployed();
+    await bscBridge.deployed();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await ethToken.setMinterAndBurnerRoles(ethBridge.address);
+    await bscToken.setMinterAndBurnerRoles(bscBridge.address);
+  });
+
+  describe("Swap", () => {
+    it("Should call swap", async () => {
+      await ethBridge.swap(
+        owner.address,
+        ethers.utils.parseEther("0.1"),
+        ETHER_CHAIN_ID,
+        BSC_CHAIN_ID,
+        await ethToken.symbol(),
+        1
+      );
+      expect(await ethToken.balanceOf(owner.address)).to.equal(
+        ethers.utils.parseEther("0.9")
+      );
+
+      // TODO: check emited event
+    });
+
+    it("Should fail if existing transaction", async () => {
+      await ethBridge.swap(
+        owner.address,
+        ethers.utils.parseEther("0.1"),
+        ETHER_CHAIN_ID,
+        BSC_CHAIN_ID,
+        await ethToken.symbol(),
+        1
+      );
+      await expect(
+        ethBridge.swap(
+          owner.address,
+          ethers.utils.parseEther("0.1"),
+          ETHER_CHAIN_ID,
+          BSC_CHAIN_ID,
+          await ethToken.symbol(),
+          1
+        )
+      ).to.be.revertedWith("Existing transaction");
+    });
+  });
+
+  describe("Redeem", () => {
+    const swapAndRedeem = async () => {
+      await ethBridge.swap(
+        owner.address,
+        ethers.utils.parseEther("0.1"),
+        ETHER_CHAIN_ID,
+        BSC_CHAIN_ID,
+        await ethToken.symbol(),
+        1
+      );
+      // Set validator. It is the backend address
+      await bscBridge.setValidator(backend.address);
+      const msg = ethers.utils.solidityKeccak256(
+        ["address", "uint256", "uint256", "uint256", "string", "uint256"],
+        [
+          owner.address,
+          ethers.utils.parseEther("0.1"),
+          ETHER_CHAIN_ID,
+          BSC_CHAIN_ID,
+          await ethToken.symbol(),
+          1,
+        ]
+      );
+      const signedMsg = await backend.signMessage(ethers.utils.arrayify(msg));
+      const { v, r, s } = ethers.utils.splitSignature(signedMsg);
+
+      await bscBridge.redeem(
+        owner.address,
+        ethers.utils.parseEther("0.1"),
+        ETHER_CHAIN_ID,
+        BSC_CHAIN_ID,
+        await ethToken.symbol(),
+        1,
+        v,
+        r,
+        s
+      );
+
+      expect(await bscToken.balanceOf(owner.address)).to.equal(
+        ethers.utils.parseEther("0.1")
+      );
+    };
+    it("Should call redeem", async () => {
+      await swapAndRedeem();
+    });
+
+    it("Should fail if send two the same transactions", async () => {
+      const v = 28;
+      const r =
+        "0x8934ae4bf1457bbc2b551f4b2226bcf0f1b52c7033570f32c1c843e9411bbb6e";
+      const s =
+        "0x5163a22691d3b38e3afec1420b264e26a1cb2a69d92fe125257d095a947a33d7";
+      await swapAndRedeem();
+      await expect(
+        bscBridge.redeem(
+          owner.address,
+          ethers.utils.parseEther("0.1"),
+          ETHER_CHAIN_ID,
+          BSC_CHAIN_ID,
+          await ethToken.symbol(),
+          1,
+          v,
+          r,
+          s
+        )
+      ).to.be.revertedWith("Existing transaction");
+    });
+
+    it("Should fail if wrong signature", async () => {
+      const v = 20;
+      const r =
+        "0x8934ae4bf1457bbc2b551f4b2226bcf0f1b52c7033570f32c1c843e9411bbb6e";
+      const s =
+        "0x5163a22691d3b38e3afec1420b264e26a1cb2a69d92fe125257d095a947a33d7";
+      await swapAndRedeem();
+      await expect(
+        bscBridge.redeem(
+          owner.address,
+          // There is changing here
+          ethers.utils.parseEther("0.2"),
+          ETHER_CHAIN_ID,
+          BSC_CHAIN_ID,
+          await ethToken.symbol(),
+          1,
+          v,
+          r,
+          s
+        )
+      ).to.be.revertedWith("Invalid signature");
+    });
+
+    it("Should fail if not admin try send the validator address", async () => {
+      await expect(
+        bscBridge.connect(acc1).setValidator(backend.address)
+      ).to.be.revertedWith("Not admin");
+    });
   });
 });
